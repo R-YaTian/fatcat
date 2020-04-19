89677021219fa05f557a9cdc7d55cb0db9366119
R-YaTian <47445484+R-YaTian@users.noreply.github.com>
2020-04-19 14:50:49 +0800

    Build with gcc 8.x and latest cmake

--- build.bat
+++ build.bat
@@ -4,16 +4,7 @@ if exist build rmdir /S /Q build
 mkdir build
 pushd build
 
-set MINGW_BIN=C:\MinGW\bin
-if not exist %MINGW_BIN% goto _msvc
-
-set PATH=%MINGW_BIN%;%PATH%
 cmake .. -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=MINSIZEREL && mingw32-make
-goto _the_end
 
-:_msvc
-cmake .. && start "" fatcat.sln
-
-:_the_end
 popd
 pause
