fcb7afeaf6848cb80de6d8870db8dd2b547f0390
R-YaTian <47445484+R-YaTian@users.noreply.github.com>
2023-04-17 16:18:55 +0800

    cmake: use c++14

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ if exist build rmdir /S /Q build
 mkdir build
 pushd build
 
-cmake .. -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=MINSIZEREL && mingw32-make
+cmake .. -G "MinGW Makefiles" -DCMAKE_BUILD_TYPE=MINSIZEREL -DCMAKE_CXX_STANDARD=14 && mingw32-make
 
 popd
 pause
